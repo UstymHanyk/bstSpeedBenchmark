50c6d5cdd94b49a39cdb484f9ffbf518f5307575
Ustym Hanyk <ustym.hanyk@ucu.edu.ua>
2021-05-12 21:48:11 +0300

    Working benchmark v1

---
+++ bst_probe.py
@@ -0,0 +1,16 @@
+"""
+File: bst_probe.py
+
+A tester program for binary search trees.
+"""
+
+from linkedbst import LinkedBST
+
+
+def main():
+    tree = LinkedBST()
+    tree.demo_bst("words.txt")
+
+
+if __name__ == "__main__":
+    main()
